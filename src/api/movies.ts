d79cea7a65e6b25c5df487e5468de920cc6458b4
Paulo Santos <paulo.jorge.ngs@gmail.com>
2025-04-23 02:39:31 +0100

    MRA-4-feat: add axios npm package and create an axios instance under the new api directory

---
+++ src/api/movies.ts
@@ -0,0 +1,9 @@
+import { api } from './axios';
+
+export const getAllMovies = () => {};
+
+export const getMovieById = () => {};
+
+export const getMoviesByTopRevenue = () => {};
+
+export const getTopRevenueByYear = () => {};
